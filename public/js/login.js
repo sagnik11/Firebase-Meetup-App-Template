743b8b71038c183c8e160b883e55e4cc67e14161
Sagnik Ghosh <sagnik@worqhat.com>
2022-11-10 08:24:39 +0530

    Complete the Login Page

---
+++ public/js/login.js
@@ -0,0 +1,14 @@
+var loginEmail = document.getElementById("loginEmail").value;
+var loginPassword = document.getElementById("loginPassword").value;
+
+document.getElementById("submitLogin").addEventListener("click", function() {
+    // Handle the Login submission and redirect over here
+});
+
+document.getElementById("googleLogin").addEventListener("click", function() {
+    // Handle the Google Login and redirect over here
+});
+
+document.getElementById("forgotPassword").addEventListener("click", function() {
+    // Handle the Forgot Password and redirect over here
+});
