c37f7724f2fc8b3be917e182d1e38dbf2c783cc6
Sagnik Ghosh <sagnik@worqhat.com>
2022-11-10 08:25:48 +0530

    Create the Signup Process Page

---
+++ public/js/signup.js
@@ -0,0 +1,10 @@
+var signupEmail = document.getElementById("signupEmail").value;
+var signupPassword = document.getElementById("signupPassword").value;
+
+document.getElementById("submitSignup").addEventListener("click", function() {
+    // Handle the Signup submission and redirect over here
+});
+
+document.getElementById("googleSignup").addEventListener("click", function() {
+    // Handle the Google Signup and redirect over here
+});
