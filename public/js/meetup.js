736e9ff39bcd83980f6b33e2647bf3f2bf292409
Sagnik Ghosh <sagnik@worqhat.com>
2022-11-10 19:57:27 +0530

    Complete the template for Firebase

---
+++ public/js/meetup.js
@@ -0,0 +1,34 @@
+// Handle the Meetup RSVP submission
+function submitRSVP() {
+
+}
+
+// Handle the Meetup RSVP submission display on Button Click (Basic JS)
+function displayRSVPChoice(choice) {
+    if (choice == "yes") {
+
+    } else if (choice == "no") {
+
+
+    }
+}
+
+// Handle the Meetup RSVP Submission on page load by fetching from the Database
+function fetchRSVP() {
+
+}
+
+// Display the user details from the signin path to display the signed in email id
+function displayUserDetails() {
+
+}
+
+// Display the real-time display of attendants
+function displayAttendants() {
+
+}
+
+//Display the real time display of conversations
+function displayConversations() {
+
+}
