6145a426874cc8f042804e8721be8adc0b4f3948
Sagnik Ghosh <sagnik@worqhat.com>
2022-11-14 21:59:01 +0530

    Complete Google Authentication

--- public/js/signup.js
+++ public/js/signup.js
@@ -19,6 +19,33 @@ document.getElementById("submitSignup").addEventListener("click", function() {
 
 });
 
+var provider = new firebase.auth.GoogleAuthProvider();
+
 document.getElementById("googleSignup").addEventListener("click", function() {
     // Handle the Google Signup and redirect over here
+
+    firebase.auth()
+        .signInWithPopup(provider)
+        .then((result) => {
+            console.log("result", result);
+
+            var credential = result.credential;
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            var token = credential.accessToken;
+            console.log("token", token);
+            // The signed-in user info.
+            var user = result.user;
+            console.log("user", user);
+
+        }).catch((error) => {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            console.log("error", errorCode, errorMessage, email, credential);
+        });
+
 });
