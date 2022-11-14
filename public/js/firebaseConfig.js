d252e43acabc2dac6d1d7d1ca016ff94399115df
Sagnik Ghosh <sagnik@worqhat.com>
2022-11-14 21:41:47 +0530

    Initialize Firebase

---
+++ public/js/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCr2yhhjBMrCohl8fIJvNqLStMjeB1usmo",
+    authDomain: "fir-meetup-template.firebaseapp.com",
+    projectId: "firebase-meetup-template",
+    storageBucket: "firebase-meetup-template.appspot.com",
+    messagingSenderId: "120951307608",
+    appId: "1:120951307608:web:22f29768e29d950a0af92d"
+};
+
+const app = initializeApp(firebaseConfig);
